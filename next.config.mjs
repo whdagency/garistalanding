b67499475a4ec9dc1c9f842bd3eec87aa838a94c
whdagency <138483447+whdagency@users.noreply.github.com>
2024-04-14 20:49:53 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,17 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/menudemo',
+        destination: '/',
+        permanent: true,
+      },
+      
+    ]
+  },
+}
