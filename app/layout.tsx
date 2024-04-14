a373210e630ac0da80eedc80805899ebd817b74d
whdagency <138483447+whdagency@users.noreply.github.com>
2024-04-14 15:40:52 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ const myLocalFont = localFont({
 
 export const metadata: Metadata = {
   title: "Garista",
-  description: "The Ultimate digital menu solution for Restaurants and Cafes",
+  description: "The Ultimate digital menu solution",
 };
 
 export default function RootLayout({
